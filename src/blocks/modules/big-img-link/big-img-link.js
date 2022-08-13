9d9d53b2dc31be2a7fd6889fadc440a5c9d9bd7b
eugene <eugenefromrus@gmail.com>
2022-08-13 17:38:36 +1000

    Промежуточный

---
+++ src/blocks/modules/big-img-link/big-img-link.js
@@ -0,0 +1,13 @@
+modules.define('big-img-link', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
