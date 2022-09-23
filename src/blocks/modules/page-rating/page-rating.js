f581dd371f253a5fda2a36dee171eaf7b86d62c7
eugene <eugenefromrus@gmail.com>
2022-09-23 16:41:03 +1000

    Верстка страницы с рейтингом

---
+++ src/blocks/modules/page-rating/page-rating.js
@@ -0,0 +1,13 @@
+modules.define('page-rating', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
